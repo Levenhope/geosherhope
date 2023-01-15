d0557004d23fe64be1d4638aae29f6d0a4fde690
Nadezhda L <levenhope@gmail.com>
2023-01-15 18:06:39 +0300

    main source and constants

---
+++ src/const.js
@@ -0,0 +1,18 @@
+export const CARD_MODE = {
+  DEFAULT: `DEFAULT`,
+  EDIT: `EDIT`
+};
+
+export const COLORS = [
+  `black`,
+  `yellow`,
+  `green`,
+  `blue`,
+  `red`
+];
+
+export const BLANK_CARD = {
+  color: COLORS[0],
+  description: ``,
+  isArchive: false
+};
